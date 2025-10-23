42a4957845581c899e11181c678bd5ff3397e5e8
Sainagu-H9 <nagu0983uv@gmail.com>
2025-10-23 13:32:50 +0530

    Refactor bill splitter program for readability
    Updated variable names and print statements for clarity.

--- day2_ smart bill splitter.c
+++ day2_ smart bill splitter.c
@@ -1,21 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    float mealcost,totalbill,person1_share,person2_share,person3_share;
-    float tax=0.05,tip=0.10;
-    printf("enter the mealcost:");
-    scanf("%f",&mealcost);
-    totalbill=mealcost+(mealcost*tax)+(mealcost*tip);
-    printf("totalbill:%f\n",totalbill);
-    person1_share=totalbill/2;//sharing person3_share
-    person2_share=totalbill/2;//sharing person3_share
-    person3_share=0;// forgot wallet
-    printf("each person should pay as follows:\n");
-    printf("person1_share:%f\n",person1_share);
-    printf("person2_share:%f\n",person2_share);
-    printf("person3_share:%f\n",person3_share);
+    float mc,t_bill,p1_share,p2_share,p3_share;
+    float tax=0.05,tip=0.1;
+
+    printf("--------Enter the mealcost--------::");
+    scanf("%f",&mc);
+
+    t_bill = mc+(mc*tax)+(mc*tip);
+    printf("-------Total Bill is ::%2f\n",t_bill);
+
+    p1_share = t_bill/2;
+    p2_share = t_bill/2;
+    p3_share = 0;
+
+    printf("-------Bill Splitin amoung the 3 people--------\n");
+    printf("person1_share is %.2f\n",p1_share);
+    printf("person2_share is %.2f\n",p2_share);
+    printf("person3_share is %.2f\n",p3_share);
+
     return 0;
 }
-
-
-    
