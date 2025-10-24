4ccdee3c0ed4f4800e4fc3658a498d930a15596d
Sainagu-H9 <nagu0983uv@gmail.com>
2025-10-24 16:08:48 +0530

    Update Day3_ElectricityBillCalculator.c

--- Day3_ElectricityBillCalculator.c
+++ Day3_ElectricityBillCalculator.c
@@ -2,23 +2,23 @@
 int main()
 {
     float uc,tb,fm=50.00;
-    printf("enter a number::");
+    printf("enter number of units ::");
     scanf("%f",&uc);
 
     if(uc<=100)
     {
         tb=uc*3.00;
-    }else if(uc<=101 && uc<=200)
+    }else if(uc>=101 && uc<=200)
     {
         tb=uc*4.50;
-    }else if (uc<=201 && uc<=300)
+    }else if (uc>=201 && uc<=300)
     {
         tb=uc*6.00;
     }else{
         tb=uc*8.00;
     }
-    tb+50;
-    printf("------Electricity bill -------");
+    tb+=50;
+    printf("------Electricity bill -------\n");
     printf("Total Cost for the %f is:::%.2f ",uc,tb);
     return 0;
 }
