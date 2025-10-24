d95489d3d4303f1c3249b201e2f89478d93b0112
Sainagu-H9 <nagu0983uv@gmail.com>
2025-10-24 15:38:57 +0530

    Implement electricity bill calculator
    Calculate electricity bill based on usage.

---
+++ Day3_ElectricityBillCalculator.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    float uc,tb,fm=50.00;
+    printf("enter a number::");
+    scanf("%f",&uc);
+
+    if(uc<=100)
+    {
+        tb=uc*3.00;
+    }else if(uc<=101 && uc<=200)
+    {
+        tb=uc*4.50;
+    }else if (uc<=201 && uc<=300)
+    {
+        tb=uc*6.00;
+    }else{
+        tb=uc*8.00;
+    }
+    tb+50;
+    printf("------Electricity bill -------");
+    printf("Total Cost for the %f is:::%.2f ",uc,tb);
+    return 0;
+}
